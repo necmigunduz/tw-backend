7153b2ae01d42f4654b9ac52da0e6cb331a79a5d
necmigunduz <necmigunduz@gmail.com>
2022-01-23 18:58:03 +0300

    Fix spec helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,9 @@
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 
+# require database cleaner at the top level
+require 'database_cleaner'
+
 require File.expand_path('../config/environment', __dir__)
 
 # Prevent database truncation if the environment is production
@@ -32,6 +35,15 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
+# configure shoulda matchers to use rspec as the test framework and full matcher libraries for rails
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -60,4 +72,20 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+    # add `FactoryBot` methods
+    config.include FactoryBot::Syntax::Methods
+
+    # start by truncating all the tables but then use the faster transaction strategy the rest of the time.
+    config.before(:suite) do
+      DatabaseCleaner.clean_with(:truncation)
+      DatabaseCleaner.strategy = :transaction
+    end
+  
+    # start the transaction strategy as examples are run
+    config.around(:each) do |example|
+      DatabaseCleaner.cleaning do
+        example.run
+      end
+    end
 end
